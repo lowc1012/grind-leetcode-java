5b031313d3e86e99dc2d4ff06a88a5418cb2e1ba
Ryan Lo <lowc1012@gmail.com>
2022-09-19 00:36:35 +0800

    15.3-sum.java

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -7,7 +7,31 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+        if (nums.length < 3) {
+            return Collections.emptyList();
+        }
+        Set<List<Integer>> result = new HashSet<>();
+
+        Arrays.sort(nums);
+        int sum = 0;
+        // time complexity: O(n^2), space complexity: O(n)
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return new ArrayList<>(result);
     }
 }
 // @lc code=end
